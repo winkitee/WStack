a3d93952b94104cb455c16a875e4858383d4580c
winkitee <winkitee@gmail.com>
2023-04-19 18:10:01 +0900

    feat: Modified to not insert the last element if the count is the same.

--- Tests/WStackTests/WStackTests.swift
+++ Tests/WStackTests/WStackTests.swift
@@ -6,6 +6,5 @@ final class WStackTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(WStack().text, "Hello, World!")
     }
 }
